447b603be500b8c968b209730949e3db25b6a771
Mauricio Lacerda <mauricio.lacerda@pm.me>
2022-03-29 17:54:47 -0300

    Create fotografia content-type

---
+++ src/api/fotografia/services/fotografia.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * fotografia service.
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::fotografia.fotografia');
