cd65d858fb0962f8b4903eb14daac63c1e5a430a
Mauricio Lacerda <mauricio.lacerda@pm.me>
2022-09-05 18:04:20 -0300

    Change admin panel url

--- config/admin.js
+++ config/admin.js
@@ -1,5 +1,6 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET', 'c839b0bbcb7019fb9edd8755db3131a5'),
+    secret: env("ADMIN_JWT_SECRET", "c839b0bbcb7019fb9edd8755db3131a5"),
   },
+  url: "/dashboard", // localhost:1337/dashboard
 });
