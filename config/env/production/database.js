176f71d7b798dced1bf31b981ed01c85fd5c5a79
Mauricio Lacerda <mauricio.lacerda@pm.me>
2022-11-09 00:31:11 -0300

    Refactor prod database connection config

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,18 +1,36 @@
 // path: ./config/env/production/database.js
-const { parse } = require("pg-connection-string");
+// const { parse } = require("pg-connection-string");
+
+// module.exports = ({ env }) => {
+//   const { host, port, database, user, password } = parse(env("DATABASE_URL"));
+
+//   return {
+//     connection: {
+//       client: "postgres",
+//       connection: {
+//         host,
+//         port,
+//         database,
+//         user,
+//         password,
+//       },
+//       debug: false,
+//     },
+//   };
+// };
+
+// path: ./config/env/production/database.js
 
 module.exports = ({ env }) => {
-  const { host, port, database, user, password } = parse(env("DATABASE_URL"));
-
   return {
     connection: {
       client: "postgres",
       connection: {
-        host,
-        port,
-        database,
-        user,
-        password,
+        host: env("DATABASE_HOST"),
+        port: env("DATABASE_PORT"),
+        database: env("DATABASE_NAME"),
+        user: env("DATABASE_USERNAME"),
+        password: env("DATABASE_PASSWORD"),
       },
       debug: false,
     },
