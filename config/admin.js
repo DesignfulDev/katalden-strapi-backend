eb541794e2e46990b307587a9ec5a78ed8be97f6
Mauricio Lacerda <mauricio.lacerda@pm.me>
2022-11-09 00:40:37 -0300

    Add required apiToken salt to admin config

--- config/admin.js
+++ config/admin.js
@@ -3,4 +3,7 @@ module.exports = ({ env }) => ({
     secret: env("ADMIN_JWT_SECRET", "c839b0bbcb7019fb9edd8755db3131a5"),
   },
   url: "/dashboard", // localhost:1337/dashboard
+  apiToken: {
+    salt: env("API_TOKEN_SALT"),
+  },
 });
